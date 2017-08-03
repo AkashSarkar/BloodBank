dce4c91ad30af7d9700ee66d09fdc0daf4fe2c20
Akash Sarkar <akashsarker1433@gmail.com>
2017-08-03 23:19:09 +0600

    cordova

--- www/build/3.main.js
+++ www/build/3.main.js
@@ -6,7 +6,7 @@ webpackJsonp([3],{
 "use strict";
 Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_ionic_angular__ = __webpack_require__(8);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_ionic_angular__ = __webpack_require__(6);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__profile__ = __webpack_require__(53);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "ProfilePageModule", function() { return ProfilePageModule; });
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
